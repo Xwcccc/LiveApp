38b198cccfa01d49cb5a0a6806ddbb8966b93fa6
Xwcccc <96057280+Xwcccc@users.noreply.github.com>
2023-07-03 17:37:33 +0800

    Update SendNoteUtil.java

--- src/main/java/com/zsxy/utils/SendNoteUtil.java
+++ src/main/java/com/zsxy/utils/SendNoteUtil.java
@@ -20,7 +20,7 @@ public class SendNoteUtil {
 
     private static final String dysmsapi = "dysmsapi.aliyuncs.com";
     private String code;
-    DefaultProfile profile = DefaultProfile.getProfile("default", "LTAI5tDCpzRViraLKdSmpd3z", "b5biV3TdACcACO4Bl0O9GL3ViYya2Y");
+    DefaultProfile profile = DefaultProfile.getProfile("default", "*****", "*****");
     IAcsClient client = new DefaultAcsClient(profile);
 
     public String getCode() {
@@ -44,16 +44,16 @@ public class SendNoteUtil {
         //接受验证码的手机号
         request.putQueryParameter("PhoneNumbers", PhoneNumbers);
         //签名
-        request.putQueryParameter("SignName", "HitszLiveApp");
+        request.putQueryParameter("SignName", "*****");
         //模板代码
-        request.putQueryParameter("TemplateCode", "SMS_461380262");
+        request.putQueryParameter("TemplateCode", "******8");
         //用户定义的验证码内容
         request.putQueryParameter("TemplateParam","{\"code\":"+"\""+code+"\"}");
         try {
             CommonResponse response = client.getCommonResponse(request);
             String returnStr = response.getData();
             System.out.println(returnStr);
-            JSONObject jsonObject = JSONUtil.parseObj(returnStr);
+            JSONObject jsonObject = JSONUtil.parseObj(returnStr);    
             //返回的信息
             return jsonObject.getStr("Message");
         } catch (ServerException e) {
